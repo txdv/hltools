c05905551304e9594a7c4516319ee388f16e8b88
Andrius Bentkus <andrius.bentkus@gmail.com>
2011-04-12 06:02:56 +0200

    Add file checker to verifier.
    Checks for files: sounds, sprites, models.

--- Verifier.Test/Main.cs
+++ Verifier.Test/Main.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Collections.Generic;
 using HLTools;
 
 namespace HLTools.Test
@@ -13,19 +14,57 @@ namespace HLTools.Test
 			int badpoints = 0;
 			
 			v.MalformedWadFile += delegate(string wadfile) {
-				Console.WriteLine ("  The referenced wad file is malformed: {0}", wadfile);
+				Console.WriteLine("  The referenced wad file is malformed: {0}", wadfile);
 				badpoints += 1;
 			};
 			
 			v.MisnamedModDir += delegate(string wadfile) {
-				Console.WriteLine ("  Mod dir does not exist: {0}", wadfile);
+				Console.WriteLine("  Mod dir does not exist: {0}", wadfile);
 				badpoints += 2;
 			};
 			
+			List<string> missingWads = new List<string>();
+			
 			v.FileNotExistent += delegate(string wadfile) {
-				Console.WriteLine ("  File does not exist: {0}", wadfile);
+				Console.WriteLine("  File does not exist: {0}", wadfile);
+				missingWads.Add(wadfile);
 				badpoints += 4;
 			};
+			
+			List<string> missingFiles = new List<string>();
+			
+			v.MissingTextures += delegate(string[] missingTextures) {
+				if (missingTextures.Length == 0) {
+					return;
+				}
+				
+				foreach (string missingTexture in missingTextures) {
+					Console.WriteLine("  {0}", missingTexture);
+				}
+				
+				foreach (string missingWad in missingWads) {
+					Console.WriteLine("  {0}", missingWad);
+				}
+			};
+			
+			v.MissingSprites += delegate(string[] missingSprites) {
+				missingFiles.AddRange(missingSprites);
+			};
+			
+			v.MissingSounds += delegate(string[] missingSounds) {
+				missingFiles.AddRange(missingSounds);
+			};
+			
+			v.MissingModels += delegate(string[] missingModels) {
+				missingFiles.AddRange(missingModels);
+			};
+			
+			if (missingFiles.Count > 0) {
+				foreach (string file in missingFiles) {
+					Console.WriteLine ("  {0}", file);	
+				}
+				System.Environment.Exit(0);
+			}
 			
 			if (args.Length > 2) {
 				v.VerifyMap(args[2]);
@@ -33,10 +72,9 @@ namespace HLTools.Test
 			} else {
 				foreach (var file in Directory.GetFiles(v.ModMapsDirectory, "*.bsp")) {
 					FileInfo fi = new FileInfo(file);
-					
-					Console.WriteLine (fi.Name);
+					Console.WriteLine(fi.Name);
 					v.VerifyMap(fi.Name);
-					Console.WriteLine (badpoints);
+					Console.WriteLine(badpoints);
 					badpoints = 0;
 				}
 			}
--- Verifier/Verifier.cs
+++ Verifier/Verifier.cs
@@ -31,13 +31,17 @@ namespace HLTools
 		{
 			return Path.Combine(GetModDir(filename), GetBareFilename(filename));
 		}
-		
 	}
 	
 	public class ValveFile
 	{
 		public ValveFile(string moddir, string file)
 		{
+			// HACK: this happens, apparently /models/bla.mdl = models/bla.mdl
+			if (file.StartsWith("/")) {
+				file = file.Substring(1);	
+			}
+			
 			ModDirectory = moddir;
 			File = file;
 		}
@@ -78,9 +82,11 @@ namespace HLTools
 	{
 		public readonly string ValveDir = "valve";
 		public readonly string MapDir = "maps";
+		public readonly string SoundDir = "sound";
 		public readonly string[] SkyEndings = new string[] { "bk", "ft", "dn", "up" , "rt", "lf" };
 		public readonly string[] SkyExtensions = new string[] { ".bmp", ".tga" };
 		
+		public string ModDir { get; protected set; }
 		public string BaseDirectory { get; protected set; }
 		public string ModDirectory { get; protected set; }
 		
@@ -91,7 +97,8 @@ namespace HLTools
 		public Verifier(string basedir, string mod)
 		{
 			BaseDirectory = basedir;
-			ModDirectory = Path.Combine(basedir, mod);
+			ModDir = mod;
+			ModDirectory = Path.Combine(basedir, ModDir);
 			ModMapsDirectory = Path.Combine(ModDirectory, MapDir);
 		}
 		
@@ -124,6 +131,29 @@ namespace HLTools
 			return (potential == null ? null : potential);
 		}
 		
+		public bool FileExists(ValveFile file)
+		{
+			string path = Path.Combine(BaseDirectory, file.ToString());
+			return File.Exists(path);
+		}
+		
+		public ValveFile GetFile(string path)
+		{
+			ValveFile file = null;
+			
+			file = new ValveFile(ModDir, path);
+			if (FileExists(file)) {
+				return file;
+			}
+			
+			file = new ValveFile(ValveDir, path);
+			if (FileExists(file)) {
+				return file;
+			}
+			
+			return null;
+		}
+		
 		public string[] Seperate(string wad)
 		{
 			List<string> list = new List<string>();
@@ -137,9 +167,22 @@ namespace HLTools
 		}
 		
 		public delegate void MalformedWadFileDelegate(string wadfile);
+		public delegate void MissingTexturesDelegate(string[] missingTextures);
+		public delegate void FileArrayDelegate(string[] UsedTextures);
+		
 		public event MalformedWadFileDelegate MalformedWadFile;
 		public event MalformedWadFileDelegate MisnamedModDir;
 		public event MalformedWadFileDelegate FileNotExistent;
+		public event MissingTexturesDelegate MissingTextures;
+		
+		public event FileArrayDelegate UsedSprites;
+		public event FileArrayDelegate MissingSprites;
+		
+		public event FileArrayDelegate UsedSounds;
+		public event FileArrayDelegate MissingSounds;
+		
+		public event FileArrayDelegate UsedModels;
+		public event FileArrayDelegate MissingModels;
 		
 		public void VerifyMap(string map)
 		{
@@ -206,6 +249,68 @@ namespace HLTools
 					}
 				}
 				
+				#region Sprites
+				
+				List<string> sprites = new List<string>();
+				if (ent.ContainsKey("env_sprite")) {
+					foreach (var dict in ent["env_sprite"]) {
+						string model = dict["model"];
+						if (!sprites.Contains(model)) {
+							sprites.Add(model);
+						}
+					}
+				}
+				if (UsedSprites != null) {
+					UsedSprites(sprites.ToArray());
+				}
+				List<string> missingSprites = new List<string>();
+				foreach (string sprite in sprites) {
+					ValveFile file = GetFile(sprite);
+					if (file == null) {
+						missingSprites.Add(sprite);
+					}
+				}
+				if (MissingSprites != null) {
+					MissingSprites(missingSprites.ToArray());	
+				}
+				
+				#endregion
+				
+				#region Sounds
+				
+				List<string> sounds = new List<string>();
+				if (ent.ContainsKey("ambient_generic")) {
+					foreach (var ambient in ent["ambient_generic"]) {
+						if (ambient.ContainsKey("message")) {
+							string message = Path.Combine(SoundDir, ambient["message"]);
+							if (!sounds.Contains(message)) {
+								sounds.Add(message);
+							}
+						}
+					}
+				}
+				
+				if (UsedSounds != null) {
+					UsedSounds(sounds.ToArray());
+				}
+				
+				List<string> missingSounds = new List<string>();
+				
+				foreach (string sound in sounds) {
+					ValveFile file = GetFile(sound);
+					if (file == null) {
+						missingSounds.Add(sound);
+					}
+				}
+				
+				if (MissingSounds != null) {
+					MissingSounds(missingSounds.ToArray());
+				}
+				
+				#endregion
+				
+				#region Textures
+				
 				List<string> existingFilenames = new List<string>();
 				foreach (var wadFile in existingWads) {
 					WADParser wp = new WADParser(File.OpenRead(wadFile.ToString(BaseDirectory)));
@@ -215,13 +320,55 @@ namespace HLTools
 					wp.LoadFiles();
 				}
 				
-				int missingTextures = 0;
+				List<string> missingTextures = new List<string>();
+					
 				foreach (var item in textureList) {
 					if (!existingFilenames.Contains(item.ToLower())) {
-						Console.WriteLine (item);
-						missingTextures++;
+						missingTextures.Add(item);
 					}
 				}
+				
+				if (MissingTextures != null) {
+					MissingTextures(missingTextures.ToArray());
+				}
+				
+				#endregion
+				
+				#region Models
+				
+				List<string> models = new List<string>();
+				foreach (var entityPair in ent) {
+					foreach (var entity in entityPair.Value) {
+						if (entity.ContainsKey("model")) {
+							string model = entity["model"];
+							if (!model.EndsWith(".mdl")) {
+								continue;
+							}
+							if (!models.Contains(model)) {
+								models.Add(model);
+							}
+						}
+					}
+				}
+				
+				if (UsedModels != null) {
+					UsedModels(models.ToArray());
+				}
+				
+				List<string> missingModels = new List<string>();
+				
+				foreach (var model in models) {
+					ValveFile file = GetFile(model);
+					if (file == null) {
+						missingModels.Add(model);
+					}
+				}
+				
+				if (MissingModels != null) {
+					MissingModels(missingModels.ToArray());
+				}
+				
+				#endregion
 			}
 		}
 	}
