b8e6aabdc95a7c236c92aff6e361f0be44dae66a
Andrius Bentkus <andrius.bentkus@gmail.com>
2012-12-14 00:49:05 +0100

    Change license.

--- HLTools/BSPParser.cs
+++ HLTools/BSPParser.cs
@@ -1,24 +1,3 @@
-// 
-//     This file is part of Iron.
-//     Iron is an extensive game engine written in C# aiming to
-//     utilize already exitent game content 
-// 
-//     Copyright (C) 2010  Andrius Bentkus
-//     Copyright (C) 2010  Giedrius Graževičius
-// 
-//     Iron is free software: you can redistribute it and/or modify
-//     it under the terms of the GNU General Public License as published by
-//     the Free Software Foundation, either version 3 of the License, or
-//     (at your option) any later version.
-// 
-//     Iron is distributed in the hope that it will be useful,
-//     but WITHOUT ANY WARRANTY; without even the implied warranty of
-//     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//     GNU General Public License for more details.
-// 
-//     You should have received a copy of the GNU General Public License
-//     along with Iron.  If not, see <http://www.gnu.org/licenses/>.
-//
 using System;
 using System.IO;
 using System.Text;
---
+++ README.md
@@ -0,0 +1,10 @@
+HLTools
+=======
+
+This is a C# library to manage GoldSrc specific files.
+
+License
+=======
+
+The library is available under the [MIT license](http://en.wikipedia.org/wiki/MIT_License).
+Note that the actual library is not licensed under the MIT license.
